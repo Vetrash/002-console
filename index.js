ff5eaf7d24af063cb85a0d78aeeb0eee1258155f
Alex <a.emelyanov@petrosoft.su>
2024-07-04 21:04:08 +0300

    add task #5

--- index.js
+++ index.js
@@ -1,102 +1,33 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
+import axios from "axios";
+import rl from "readline-sync";
 
-// Определение команды 'current'
+const getWeather = async () => {
+  let city = "";
+  console.log(
+    `Погоду в каком городе вы хотите узнать?
+Введите название города на английском языке`
+  );
+  city = rl.question("city: ");
 
-// const argvCurrent = yargs(hideBin(process.argv))
-//  .command('current', 'Получить текущую дату и время', {
-//     year: {
-//       alias: 'y',
-//       describe: 'Год',
-//       type: 'number',
-//     },
-//     month: {
-//       alias: 'm',
-//       describe: 'Месяц',
-//       type: 'number',
-//     },
-//     date: {
-//       alias: 'd',
-//       describe: 'Дата в календарном месяце',
-//       type: 'number',
-//     },
-//   }).argv
-
-
-//   const currentDate = new Date();
-//   if (argvCurrent.year || argvCurrent.y) {
-//     console.log(currentDate.getFullYear());
-//   }
-//   if (argvCurrent.month || argvCurrent.m) {
-//     console.log(currentDate.getMonth() + 1); 
-//   }
-//   if (argvCurrent.date || argvCurrent.d) {
-//     let dd = currentDate.getDate(); 
-//     let mm = currentDate.getMonth() + 1; 
-//     const yyyy = currentDate.getFullYear(); 
-
-//     if( dd < 10) {
-//         dd = '0' + dd;
-//     }
-//     if (mm < 10) {
-//         mm = '0' + mm;
-//     }
-//     console.log( `${dd}.${mm}.${yyyy}`);
-//   }
-
-
-// Определение команд 'add' и 'sub'
-const argvAdd = yargs(hideBin(process.argv)).command('add', 'Добавить количество дней или месяцев к текущей дате', {
-  days: {
-    alias: 'd',
-    describe: 'Количество дней',
-    type: 'number',
-  },
-  months: {
-    alias: 'm',
-    describe: 'Количество месяцев',
-    type: 'number',
-  },
-}).argv
-
-
-
-
-if (argvAdd.date || argvAdd.d) {
-
-  const daysAdd = argvAdd.date ? argvAdd.date : argvAdd.d;
-
-  const nextDate =new Date( Date.now() + Number(daysAdd)*86400000);
-  let dd = nextDate.getDate(); 
-  let mm = nextDate.getMonth() + 1; 
-  const yyyy = nextDate.getFullYear(); 
-
-  if( dd < 10) {
-      dd = '0' + dd;
+  if (city.length < 1) {
+    console.log("Пожалуйста, введите город");
+    getWeather();
+    return;
   }
-  if (mm < 10) {
-      mm = '0' + mm;
+
+  const myAPIKey = process.env.myAPIKey;
+  const url = `https://api.weatherapi.com/v1/current.json?q=${city}&key=${myAPIKey}`;
+
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+    return;
+  } catch (error) {
+    console.error(error);
+    return;
   }
-  console.log( `${dd}.${mm}.${yyyy}`);
-}
+};
 
-// const argSub = yargs.command('sub', 'Вычесть количество дней или месяцев из текущей даты', {
-//   days: {
-//     alias: 'd',
-//     describe: 'Количество дней',
-//     type: 'number',
-//   },
-//   months: {
-//     alias: 'm',
-//     describe: 'Количество месяцев',
-//     type: 'number',
-//   },
-// }).argv;
-
-
-
-
-// // Запуск утилиты
-// yargs.parse(process.argv.slice(2));
+getWeather();
